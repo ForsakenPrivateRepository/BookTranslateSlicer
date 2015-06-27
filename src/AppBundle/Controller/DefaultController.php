455ad80396d8c6af3e1be9e578aea40a57dee91d
ReenExe <ipourri@gmail.com>
2015-06-27 23:13:19 +0300

    adding

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Document\Book;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Doctrine\ODM\MongoDB\DocumentManager;
@@ -15,6 +16,12 @@ class DefaultController extends Controller
     {
         /* @var $dm DocumentManager */
         $dm = $this->get('doctrine_mongodb')->getManager();
+
+        $book = new Book();
+        $book->setName('First Book');
+
+        $dm->persist($book);
+        $dm->flush();
 
         return $this->render('default/index.html.twig');
     }
