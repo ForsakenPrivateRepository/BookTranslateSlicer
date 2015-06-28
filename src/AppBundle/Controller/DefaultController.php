e4c4e81f1735113c688f81022d3ea2a1da95d37f
ReenExe <ipourri@gmail.com>
2015-06-28 07:56:55 +0300

    simple adding by form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,10 +3,13 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Document\Book;
+use AppBundle\Type\BookType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\LoggableCursor;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -49,7 +52,32 @@ class DefaultController extends Controller
      */
     public function formAction()
     {
-        return $this->redirect('/');
+        $form = $this->createForm(new BookType(), null, ['action' => $this->generateUrl('submit')]);
+
+        return $this->render('default/form.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/submit", name="submit")
+     */
+    public function submitAction(Request $request)
+    {
+        $book = new Book();
+
+        $form = $this->createForm(new BookType(), $book);
+
+        $form->submit($request);
+
+        /* @var $dm DocumentManager */
+        $dm = $this->get('doctrine_mongodb')->getManager();
+
+        $dm->persist($book);
+        $dm->flush();
+
+        // db.Book.count();
+        $count = $dm->getConnection()->selectDatabase('test')->selectCollection('Book')->count();
+
+        return $this->render('default/index.html.twig', ['count' => $count]);
     }
 
     /**
@@ -69,5 +97,15 @@ class DefaultController extends Controller
         $count = $books->count();
 
         return $this->render('default/index.html.twig', ['count' => $count]);
+    }
+
+    /**
+     * @Route("/translate/{word}", name="translate")
+     */
+    public function translateAction($word)
+    {
+        return new JsonResponse([
+            'translate' => $word
+        ]);
     }
 }
---
+++ src/AppBundle/Type/BookType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('save', 'submit');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Document\Book',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'book';
+    }
+}
